cbae81bfbd81595b7e08b33a1115687d4d0f0d7f
Maximiliano Sorribas <mjsorribas@gmail.com>
2020-03-25 15:51:13 -0300

    add new component tooltip

---
+++ src/components/Tooltip.vue
@@ -0,0 +1,58 @@
+<template>
+  <div :class=containerClasses>
+    <b-btn :id=buttonId :variant=buttonVariant>{{buttonText}}</b-btn>
+    <b-tooltip :ref=ref :target=target :placement=placement :title=title>
+      {{content}}
+    </b-tooltip>
+  </div>
+</template>
+<script>
+export default {
+  name: 'Tooltip',
+  props:{
+    containerClasses:{
+      type: String,
+      required: false,
+      default:''
+    },
+    buttonId:{
+      type: String,
+      required: true,
+    },
+    buttonVariant:{
+      type: String,
+      required: false,
+      default:'primary'
+    },
+    buttonText:{
+      type: String,
+      required: false,
+      default:'Button'
+    },
+    content:{
+      type: String,
+      required: true,
+    },
+    target:{
+      type: String,
+      required: true,
+    },
+    action:{
+        type:String,
+        required:false,
+    },
+    placement:{
+        type:String,
+        required:true,
+    },
+    ref:{
+        type:String,
+        required:false,
+    },
+    title:{
+        type:String,
+        required:false,
+    }
+  }
+}
+</script>
