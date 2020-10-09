13cf2679dd024654e112b0ba71d1d01848350ce1
Maximiliano Jose Sorribas <mjsorribas@gmail.com>
2020-10-09 10:29:08 -0300

    add jumbotron component

---
+++ src/components/Jumbotron.vue
@@ -0,0 +1,59 @@
+<template>
+    <b-jumbotron :header=headerData :class=classes :lead=leadData >
+        <p>{{content}}</p>
+        <b-button :v-if="withButton" :size=buttonSize :class=buttonClasses :variant=buttonVariant />
+    </b-jumbotron>
+  </b-button>
+</template>
+<script>
+export default {
+  name: 'Jumbotron',
+  props:{
+    classes:{
+      type: String,
+      required: false,
+      default:'jumbotron'
+    },
+    header:{
+      type: String,
+      required: false,
+      default:''
+    },
+    leadData:{
+      type: String,
+      required: false,
+      default:''
+    },    
+    content:{
+      type: String,
+      required: false,
+      default:''
+    },      
+    withButton:{
+      type: Boolean,
+      required: false,
+      default:false
+    },
+    buttonSize:{
+      type: String,
+      required: false,
+      default:''
+    }, 
+    buttonClasses:{
+      type: String,
+      required: false,
+      default:'btn'
+    },
+    buttonVariant:{
+      type: String,
+      required: false,
+      default:'primary'
+    },        
+  },
+  data: function () {
+    return {
+      show: true
+    }
+  }
+}
+</script>
