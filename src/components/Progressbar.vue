fad1eaba2f19bfdb06f30804289d827acd1eee09
Maximiliano Jose Sorribas <mjsorribas@gmail.com>
2020-10-28 18:00:02 -0300

    add progressbar component

---
+++ src/components/Progressbar.vue
@@ -0,0 +1,44 @@
+<template>
+    <b-progress class="classes"  :value="counter" :max="max" :show-progress :animated :show-value :variant="variant"></b-progress>
+
+</template>
+<script>
+export default {
+ name: 'ProgressBar',
+  props:{
+    align:{
+      type: String,
+      required: false,
+      default:''
+    },
+    classes:{
+      type: String,
+      required: false,
+      default:''
+    },
+    value:{
+      type: Number,
+      required: false,
+      default:''
+    },    
+    counter:{
+      type: String,
+      required: false,
+      default:''
+    },  
+    perPage:{
+      type: String,
+      required: false,
+      default:''
+    },              
+  },
+  data: function () {
+    return {
+      show: true
+    }
+  }
+}
+</script>    
+    
+}
+</script>
